ff26a0f785b8fa778155dbd08d30d577ea170be6
ghanshyam <ghanshyamanand1989@gmail.com>
2015-08-23 21:21:34 +0530

    changes made in production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,9 @@
+set :stage, :production
+
+# Replace 127.0.0.1 with your server's IP address!
+server '104.131.123.202', user: 'deploy', roles: %w{web app}
+
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
